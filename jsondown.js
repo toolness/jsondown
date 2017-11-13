a3d6066d30e981fae0d962f345625d72a5c0988c
Andre Staltz <andre@staltz.com>
2017-11-13 16:39:18 +0200

    Update memdown to v1.4.1 and update test suite
    This commit brings make this compliant with the latest Level ecosystem,
    using memdown 1.4.1 (which uses abstract-leveldown v2.7). It is an
    overall due to how memdown uses Red Black Trees as internal data
    structure, this package had to adapt in order to (de)serialize those
    trees.
    
    The test suite comes from the standard abstract-leveldown test suite,
    using tape as test runner.

--- jsondown.js
+++ jsondown.js
@@ -1,18 +1,50 @@
-var util = require('util');
-var os = require('os');
-var MemDOWN = require('memdown');
-var fs = (os.hostname() === 'runtime') ?
-  require('./runtime-fs') :
-  require('fs')
+var AbstractLevelDOWN = require("abstract-leveldown").AbstractLevelDOWN;
+var os = require("os");
+var util = require("util");
+var os = require("os");
+var MemDOWN = require("memdown");
+var fs = os.hostname() === "runtime" ? require("./runtime-fs") : require("fs");
 
+function serializeRBTree(tree) {
+  var result = {};
+  tree.forEach((key, value) => {
+    result[key] = value;
+  });
+  return result;
+}
 
-var niceStringify = require('./nice-stringify');
+function serializeStore(store) {
+  var result = {};
+  Object.keys(store).forEach(location => {
+    result[location] = serializeRBTree(store[location]);
+  });
+  return JSON.stringify(result);
+}
+
+function jsonToBatchOps(data) {
+  if (!data) {
+    throw new Error();
+  }
+  return Object.keys(data).map(function(key) {
+    var value = data[key];
+    if (typeof value != "string") {
+      try {
+        value = new Buffer(value);
+      } catch (e) {
+        throw new Error(
+          "Error parsing value " + JSON.stringify(value) + " as a buffer"
+        );
+      }
+    }
+    return { type: "put", key: key, value: value };
+  });
+};
 
 function noop() {}
 
 function JsonDOWN(location) {
-  if (!(this instanceof JsonDOWN))
-    return new JsonDOWN(location);
+  if (!(this instanceof JsonDOWN)) return new JsonDOWN(location);
+  AbstractLevelDOWN.call(this, location);
   MemDOWN.call(this, location);
   this._isLoadingFromFile = false;
   this._isWriting = false;
@@ -21,72 +53,74 @@ function JsonDOWN(location) {
 
 util.inherits(JsonDOWN, MemDOWN);
 
-JsonDOWN.prototype._jsonToBatchOps = function(data) {
-  return Object.keys(data).map(function(key) {
-    var value = data[key];
-    if (/^\$/.test(key)) {
-      key = key.slice(1);
-    } else {
-      try {
-        key = new Buffer(JSON.parse(key));
-      } catch (e) {
-        throw new Error('Error parsing key ' + JSON.stringify(key) +
-                        ' as a buffer');
-      }
-    }
-    if (typeof(value) != 'string') {
-      try {
-        value = new Buffer(value);
-      } catch (e) {
-        throw new Error('Error parsing value ' + JSON.stringify(value) +
-                        ' as a buffer');
-      }
-    }
-    return {type: 'put', key: key, value: value};
+JsonDOWN.prototype._open = function(options, callback) {
+  var self = this;
+  var loc = this.location;
+  var separator = os.platform() === "win32" ? "\\" : "/";
+  var subdir = loc
+    .split(separator)
+    .slice(0, -1)
+    .join(separator);
+
+  fs.exists(subdir, function(subdirExists) {
+    if (!subdirExists)
+      return callback(new Error(loc + ": No such file or directory"));
+
+    fs.exists(loc, function(exists) {
+      if (!exists && options.createIfMissing === false)
+        callback(new Error(loc + " does not exist (createIfMissing is false)"));
+      else if (exists && options.errorIfExists)
+        callback(new Error(loc + " exists (errorIfExists is true)"));
+      else if (!exists)
+        fs.open(loc, "w", callback);
+      else
+        fs.readFile(loc, { encoding: "utf-8", flag: "r" }, function(err, data) {
+          if (err)
+            return callback(err, data);
+          try {
+            data = JSON.parse(data);
+          } catch (e) {
+            return callback(
+              new Error("Error parsing JSON in " + loc + ": " + e.message)
+            );
+          }
+          self._isLoadingFromFile = true;
+          try {
+            try {
+              self._batch(jsonToBatchOps(data[self._location]), {}, noop);
+            } finally {
+              self._isLoadingFromFile = false;
+            }
+          } catch (e) {
+            return callback(e);
+          }
+          callback(null, self);
+        });
+    });
   });
 };
 
-JsonDOWN.prototype._open = function(options, cb) {
-  fs.readFile(this.location, {encoding: 'utf-8', flag: 'r'}, function(err, data) {
-    if (err) {
-      if (err.code == 'ENOENT') return cb(null, this);
-      return cb(err);
-    }
-    try {
-      data = JSON.parse(data);
-    } catch (e) {
-      return cb(new Error('Error parsing JSON in ' + this.location +
-                          ': ' + e.message));
-    }
-    this._isLoadingFromFile = true;
-    try {
-      try {
-        this._batch(this._jsonToBatchOps(data), {}, noop);
-      } finally {
-        this._isLoadingFromFile = false;
-      }
-    } catch (e) {
-      return cb(e);
-    }
-    cb(null, this);
-  }.bind(this));
+JsonDOWN.prototype._close = function(cb) {
+  this._writeToDisk(cb);
 };
 
 JsonDOWN.prototype._writeToDisk = function(cb) {
-  if (this._isWriting)
-    return this._queuedWrites.push(cb);
+  if (this._isWriting) return this._queuedWrites.push(cb);
   this._isWriting = true;
-  fs.writeFile(this.location, niceStringify(this._store), {
-    encoding: 'utf-8'
-  }, function(err) {
-    var queuedWrites = this._queuedWrites.splice(0);
-    this._isWriting = false;
-    if (queuedWrites.length)
-      this._writeToDisk(function(err) {
-        queuedWrites.forEach(function(cb) { cb(err); });
-      });
-    cb(err);
-  }.bind(this));
+  var self = this;
+  fs.writeFile(this.location, serializeStore(this._store), { encoding: "utf-8" },
+    function(err) {
+      var queuedWrites = this._queuedWrites.splice(0);
+      this._isWriting = false;
+      if (queuedWrites.length)
+        this._writeToDisk(function(err) {
+          queuedWrites.forEach(function(cb) {
+            cb(err);
+          });
+        });
+      cb(err);
+    }.bind(this)
+  );
 };
 
 JsonDOWN.prototype._put = function(key, value, options, cb) {
