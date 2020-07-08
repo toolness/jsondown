0baf8c6e3f7476c47b5921f2064eb32fba21b01d
garthdb <garthdb@gmail.com>
2020-07-08 11:52:48 -0600

    wip: updating dependencies

--- test.js
+++ test.js
@@ -1,29 +1,62 @@
-var test = require("tape"),
-  testCommon = require("abstract-leveldown/testCommon"),
-  jsonDOWN = require("./jsondown"),
-  testBuffer = require("fs").readFileSync(__filename),
-  db;
+const test = require("tape");
+const suite = require("abstract-leveldown/test");
+const jsonDOWN = require("./jsondown");
+const tempy = require("tempy");
 
-/*** compatibility with basic LevelDOWN API ***/
+suite({
+  test: test,
+  snapshots: false,
+  seek: false,
+  bufferKeys: false,
+  factory: function () {
+    const dir = tempy.directory();
+    return new jsonDOWN(dir);
+  },
+});
 
-require("abstract-leveldown/abstract/leveldown-test").args(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/open-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/del-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/get-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/put-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/put-get-del-test").all(jsonDOWN, test, testCommon, testBuffer);
-
-require("abstract-leveldown/abstract/batch-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/chained-batch-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/close-test").close(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/iterator-test").all(jsonDOWN, test, testCommon);
-
-require("abstract-leveldown/abstract/ranges-test").all(jsonDOWN, test, testCommon);
+// var test = require("tape"),
+//   testCommon = require("abstract-leveldown/test/common"),
+//   jsonDOWN = require("./jsondown"),
+//   testBuffer = require("fs").readFileSync(__filename),
+//   db;
+//
+// /*** compatibility with basic LevelDOWN API ***/
+//
+// // require("abstract-leveldown/test/leveldown-test").args(
+// //   jsonDOWN,
+// //   test,
+// //   testCommon
+// // );
+//
+// require("abstract-leveldown/test/open-test").all(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/del-test").all(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/get-test").all(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/put-test").all(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/put-get-del-test").all(
+//   jsonDOWN,
+//   test,
+//   testCommon,
+//   testBuffer
+// );
+//
+// require("abstract-leveldown/test/batch-test").all(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/chained-batch-test").all(
+//   jsonDOWN,
+//   test,
+//   testCommon
+// );
+//
+// require("abstract-leveldown/test/close-test").close(jsonDOWN, test, testCommon);
+//
+// require("abstract-leveldown/test/iterator-test").all(
+//   jsonDOWN,
+//   test,
+//   testCommon
+// );
+//
+// require("abstract-leveldown/test/ranges-test").all(jsonDOWN, test, testCommon);
